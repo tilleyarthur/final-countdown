c0f8e3141d7f36ac150d6bc4c09eb6188abfe916
tilleyarthur <86635878+tilleyarthur@users.noreply.github.com>
2021-08-20 20:47:04 -0400

    add some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,70 @@
-const inquiryText = document.getElementById('inquiry-text');
-const submitBtn = document.getElementById('submit-btn');
-const userScore = document.getElementById('user-score');
+// high score/time
+let scoreEl = document.querySelector("#score");
+let timeEl = document.querySelector("#time");
+let secondsLeft = 60;
 
-let currentInquiry = 0;
-let score = 0;
+var introEl = document.querySelector("#intro");
 
-var inquiry = [
+var questionsEl = document.querySelector("#questions");
+
+let questionCount = 0;
+
+var questions = [
   {
-  question: 'Commonly used data types DO NOT include___',
-  answers: ['strings', 'booleans', 'alerts', 'numbers'],
-  correctAnswer: 3,
+  title: 'Commonly used data types DO NOT include___',
+  choices: ['1. strings', '2. booleans', '3. alerts', '4. numbers'],
+  answer: 3,
   },
   {
-  question: 'The condition in an if/else statement is enclosed with____.',
-  answers: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-  correctAnswer: 2,
+  title: 'The condition in an if/else statement is enclosed with____.',
+  choices: ['1. quotes', '2. curly brackets', '3. parenthesis', '4. square brackets'],
+  answer: 3,
   },
   {
-  question: 'Arrays in JavaScript can be used to store___',
-  answers: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-  correctAnswer: 4,
+  title: 'Arrays in JavaScript can be used to store___',
+  choices: ['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above'],
+  answer: 4,
   },
   {
-  question: 'String values must be enclosed within___ when being assigned to variables.',
-  answers: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
-  correctAnswer: 3,
+  title: 'String values must be enclosed within___ when being assigned to variables.',
+  choices: ['1. commas', '2. curly brackets', '3. quotes', '4. parenthesis'],
+  answer: 3,
   },
   {
-  question: 'A very useful tool during development and debugging for printing content to the debugger is___',
-  answers: ['JavaScript', 'terminal/Bash', 'for loops', 'console.log'],
-  correctAnswer: 4
+  title: 'A very useful tool during development and debugging for printing content to the debugger is___',
+  choices: ['1. JavaScript', '2. terminal/Bash', '3. for loops', '4. console.log'],
+  answer: 4
   },
 ];
-  
-  
-  
-  
 
-submitBtn.addEventListener('click', submit);
+function startTime() {
+  let timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = `Time:${secondsLeft}s`;
 
-
-  function showInquiry(i) {
-    let questionDiv = document.getElementById('question');
-    questionDiv.textContent = i.question;
-  
-    let alts = document.querySelectorAll('.answers');
-    console.log(alts);
-    alts.forEach(function(element, index){
-      element.textContent = q.answers[index];
-  });
-};
-
-function showInquiry(i) {
-
+    if (secondsLeft === 0 || questionCount === questions.length) {
+      clearInterval(timerInterval);
+      scoreEl.textContent = secondsLeft;
+    }
+  }, 1000);
 }
 
-showInquiry (inquiry);
+function startQuiz() {
+  
+  setQuestion(questionCount);
+}
+
+function setQuestion(id) {
+  if (id < questions.length) {
+    questionsEl.textContent = questions[id].question;
+        ans1Btn.textContent = questions[id].answers[0];
+        ans2Btn.textContent = questions[id].answers[1];
+        ans3Btn.textContent = questions[id].answers[2];
+        ans4Btn.textContent = questions[id].answers[3];
+    }
+}
+startTime(60);
+startQuiz();
+
 
 
